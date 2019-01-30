f71a13c5e0f75be462365521e83007cacd1622ca
Avi Huly <avihuly@users.noreply.github.com>
2019-01-31 00:27:18 +0200

    init

---
+++ main.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import io
+from apiclient.http import MediaIoBaseDownload
+
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+	with open('token.pickle', 'rb') as token:
+		creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+	if creds and creds.expired and creds.refresh_token:
+		creds.refresh(Request())
+	else:
+		flow = InstalledAppFlow.from_client_secrets_file(
+			'credentials.json', SCOPES)
+		creds = flow.run_local_server()
+	# Save the credentials for the next run
+	with open('token.pickle', 'wb') as token:
+		pickle.dump(creds, token)
+
+drive_service = build('drive', 'v3', credentials=creds)
+
+
+file_id = '12os_T3UqgOYsIjrelcq9VyIz7gAiT3uwV2h5zaTUa74'
+fileRequest = drive_service.files().export_media(fileId=file_id,
+                                             mimeType='application/pdf')
+fh = io.BytesIO()
+downloader = MediaIoBaseDownload(fh, fileRequest)
+done = False
+while done is False:
+    status, done = downloader.next_chunk()
+    print ("Download %d%%." % int(status.progress() * 100))
